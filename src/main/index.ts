45712313876fe8d57ad8b2ee7104fd0628d2910f
rconde01 <rconde01@hotmail.com>
2026-05-19 16:37:37 -0400

    disable linux command line switches

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,11 +4,11 @@ import { dirname, join } from 'node:path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.commandLine.appendSwitch('enable-unsafe-webgpu');
-app.commandLine.appendSwitch('no-sandbox');
-app.commandLine.appendSwitch('ozone-platform', 'x11');
-app.commandLine.appendSwitch('use-angle', 'vulkan');
-app.commandLine.appendSwitch('enable-features', 'Vulkan,VulkanFromANGLE');
+// app.commandLine.appendSwitch('enable-unsafe-webgpu');
+// app.commandLine.appendSwitch('no-sandbox');
+// app.commandLine.appendSwitch('ozone-platform', 'x11');
+// app.commandLine.appendSwitch('use-angle', 'vulkan');
+// app.commandLine.appendSwitch('enable-features', 'Vulkan,VulkanFromANGLE');
 
 function createWindow(): void {
   const win = new BrowserWindow({
